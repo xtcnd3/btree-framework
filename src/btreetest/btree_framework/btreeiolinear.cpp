a632e4f67ffb4575fe039a9940a933accca3628b
xtcnd3 <btreeframework@gmail.com>
2015-08-24 19:31:35 +0100

    release 0.02 (alpha)

---
+++ src/btreetest/btree_framework/btreeiolinear.cpp
@@ -0,0 +1,62 @@
+#ifndef	BTREEIOLINEAR_CPP
+#define	BTREEIOLINEAR_CPP
+
+#include "btreeiolinear.h"
+
+template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::CBTreeLinearIO
+(
+	_t_addresstype nBlockSize, 
+	uint32_t nNumDataPools, 
+	CBTreeIOperBlockPoolDesc_t *psDataPools
+)
+	:	CBTreeIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype> (nBlockSize, nNumDataPools, psDataPools)
+	,	m_ppsPools (NULL)
+{
+	m_ppsPools = new uint8_t * [this->m_nNumDataPools];
+
+	BTREEDATA_ASSERT (m_ppsPools != NULL, "CBTreeLinearIO::CBTreeLinearIO: insufficient memory!");
+
+	uint32_t	nPool;
+
+	for (nPool = 0; nPool < this->m_nNumDataPools; nPool++)
+	{
+		m_ppsPools[nPool] = NULL;
+	}
+}
+
+template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::~CBTreeLinearIO ()
+{
+	if (m_ppsPools != NULL)
+	{
+		uint32_t	nPool;
+
+		for (nPool = 0; nPool < this->m_nNumDataPools; nPool++)
+		{
+			if (m_ppsPools[nPool] != NULL)
+			{
+				free ((void *) m_ppsPools[nPool]);
+			}
+		}
+
+		delete [] m_ppsPools;
+	}
+}
+
+template<class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+const uint8_t* CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::get_node_base (uint32_t nPool, _t_nodeiter nNode)
+{
+#if defined (_DEBUG)
+
+	BTREEDATA_ASSERT (nPool < this->m_nNumDataPools, "CBTreeLinearIO::get_node_base: nPool exceeds available pools");
+
+#endif
+
+	const uint8_t		*psPool = this->m_ppsPools[nPool];
+	const uint8_t		*psPooledNode = &(psPool[nNode * this->get_pool_total_size (nPool)]);
+
+	return (psPooledNode);
+}
+
+#endif // BTREEIOLINEAR_CPP
---
+++ src/btreetest/btree_framework/btreeiolinear.h
@@ -0,0 +1,67 @@
+#ifndef	BTREEIOLINEAR_H
+#define	BTREEIOLINEAR_H
+
+#include <string>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <stdint.h>
+#include <memory.h>
+#include <stddef.h>
+
+#if defined (WIN32)
+
+ #if defined (_HAS_MFC)
+  #include <afxtempl.h>
+ #endif
+
+ #if !defined (_HAS_MFC)
+  #include <windows.h>
+ #endif
+
+ #include <io.h>
+
+#elif defined (LINUX)
+
+ #include <string.h>
+ #include <stdlib.h>
+ #include <sys/time.h>
+ #include <sys/vfs.h>
+
+ #include <windows.h>
+
+#endif
+
+#include "btreeio.h"
+#include "btreeaux.h"
+
+template <class _t_nodeiter = uint64_t, class _t_subnodeiter = uint32_t, class _t_addresstype = uint64_t, class _t_offsettype = uint32_t>
+class CBTreeLinearIO : public CBTreeIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+{
+public:
+
+	// construction
+							CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+												(
+													_t_addresstype nBlockSize, 
+													uint32_t nNumDataPools, 
+													CBTreeIOperBlockPoolDesc_t *psDataPools
+												);
+
+							~CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+												();
+
+protected:
+
+	// address generation
+	inline const uint8_t	*get_node_base			(uint32_t nPool, _t_nodeiter nNode);
+	
+	uint8_t											**m_ppsPools;
+};
+
+#endif // BTREEIOLINEAR_H
+
+#include "btreeiolinear.cpp"
