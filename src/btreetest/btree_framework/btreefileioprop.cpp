aedd2988805d0c859713fb5487384d8b30ab0886
xtcnd3 <btreeframework@gmail.com>
2015-08-24 20:17:25 +0100

    release 0.02 (alpha)

--- src/btreetest/btree_framework/btreefileioprop.cpp
+++ src/btreetest/btree_framework/btreefileioprop.cpp
@@ -13,103 +13,35 @@
 
 #include "btreefileioprop.h"
 
-CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (const char *pszPathName, uint32_t nFileCacheNumOfLog2Lines, uint64_t nFileCacheNumOfBytesPerLine)
+CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (const char *pszPathName, uint64_t nAddrSpaceSoftLimit)
 	:	CBTreeIOproperties (CBTREE_IO_PROPERTIES_FILE)
-	,	m_nFileCacheNumOfLog2Lines (nFileCacheNumOfLog2Lines)
-	,	m_nFileCacheNumOfBytesPerLine (nFileCacheNumOfBytesPerLine)
-	,	m_nNumPaths (1)
+	,	m_nAddrSpaceSoftLimit (nAddrSpaceSoftLimit)
 {
-	m_ppStrPathNames = new string * [m_nNumPaths];
+	m_pStrPathNames = new string (pszPathName);
 
-	BTREE_ASSERT (m_ppStrPathNames != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (const char *, uint32_t , uint64_t): insufficient memory!");
-
-	m_ppStrPathNames[0] = new string (pszPathName);
-
-	BTREE_ASSERT (m_ppStrPathNames[0] != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (const char *, uint32_t , uint64_t): insufficient memory! (string)");
+	BTREE_ASSERT (m_pStrPathNames != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (const char *, uint32_t , uint64_t): insufficient memory!");
 }
 
 CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (CBTreeIOpropertiesFile &rBT)
 	:	CBTreeIOproperties (CBTREE_IO_PROPERTIES_FILE)
-	,	m_nFileCacheNumOfLog2Lines (rBT.m_nFileCacheNumOfLog2Lines)
-	,	m_nFileCacheNumOfBytesPerLine (rBT.m_nFileCacheNumOfBytesPerLine)
-	,	m_nNumPaths (rBT.m_nNumPaths)
+	,	m_nAddrSpaceSoftLimit (rBT.m_nAddrSpaceSoftLimit)
 {
-	uint32_t	i;
+	m_pStrPathNames = new string (*(rBT.m_pStrPathNames));
 
-	m_ppStrPathNames = new string * [m_nNumPaths];
-
-	BTREE_ASSERT (m_ppStrPathNames != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (CBTreeIOpropertiesFile &): insufficient memory!");
-
-	for (i = 0; i < m_nNumPaths; i++)
-	{
-		m_ppStrPathNames[i] = new string ();
-
-		BTREE_ASSERT (m_ppStrPathNames[i] != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (CBTreeIOpropertiesFile &): insuficient memory! (string)");
-
-		*(m_ppStrPathNames[i]) = *(rBT.m_ppStrPathNames[i]);
-	}
+	BTREE_ASSERT (m_pStrPathNames != NULL, "CBTreeIOpropertiesFile::CBTreeIOpropertiesFile (CBTreeIOpropertiesFile &): insufficient memory!");
 }
 
 CBTreeIOpropertiesFile::~CBTreeIOpropertiesFile ()
 {
-	uint32_t	i;
-
-	for (i = 0; i < m_nNumPaths; i++)
-	{
-		delete m_ppStrPathNames[i];
-	}
-
-	delete [] m_ppStrPathNames;
-}
-
-void CBTreeIOpropertiesFile::attach_pathname (const char *pszPathName)
-{
-	string		**ppStrPathNames = new string * [m_nNumPaths + 1];
-	uint32_t	i;
-
-	BTREE_ASSERT (ppStrPathNames != NULL, "CBTreeIOpropertiesFile::attach_pathname (const char *): insufficient memory!");
-
-	for (i = 0; i < m_nNumPaths; i++)
-	{
-		ppStrPathNames[i] = new string ();
-
-		BTREE_ASSERT (ppStrPathNames[i] != NULL, "CBTreeIOpropertiesFile::attach_pathname (CBTreeIOpropertiesFile &): insuficient memory! (string)");
-
-		*(ppStrPathNames[i]) = *(m_ppStrPathNames[i]);
-
-		delete m_ppStrPathNames[i];
-	}
-
-	ppStrPathNames[i] = new string (pszPathName);
-
-	delete [] m_ppStrPathNames;
-
-	m_ppStrPathNames = ppStrPathNames;
-
-	m_nNumPaths++;
+	delete m_pStrPathNames;
 }
 
 const char *CBTreeIOpropertiesFile::get_pathname ()
 {
-	uint32_t	n = 0;
-
-	if (m_nNumPaths > 1)
-	{
-		n = rand ();
-		n = n % m_nNumPaths;
-	}
-
-	return (m_ppStrPathNames[n]->c_str ());
+	return (m_pStrPathNames->c_str ());
 }
 
-uint32_t CBTreeIOpropertiesFile::get_num_of_log2_cache_lines ()
+uint64_t CBTreeIOpropertiesFile::get_address_space_soft_limit ()
 {
-	return (m_nFileCacheNumOfLog2Lines);
+	return (m_nAddrSpaceSoftLimit);
 }
-
-uint64_t CBTreeIOpropertiesFile::get_num_of_log2_bytes_per_cache_line ()
-{
-	return (m_nFileCacheNumOfBytesPerLine);
-}
-
-
