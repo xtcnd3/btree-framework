be40c668a5a026687f5af2ba31afeec4da45e9ee
xtcnd3 <btreeframework@gmail.com>
2016-04-06 12:27:23 +0100

    release 0.04a (alpha)

--- src/btreetest/btreekeysorttestprimitive.h
+++ src/btreetest/btreekeysorttestprimitive.h
@@ -19,16 +19,122 @@
 #include "btreetestcommon.h"
 #include "btreetestkeysort.h"
 
-typedef enum
-{
-	BTREETEST_KEYSORT_PRIMITIVE_DESCEND_KEY, 
-	BTREETEST_KEYSORT_PRIMITIVE_ASCEND_KEY, 
-	BTREETEST_KEYSORT_PRIMITIVE_RANDOM_KEY, 
-	BTREETEST_KEYSORT_PRIMITIVE_CONST_KEY
-} btreetest_keysort_primitive_seek_e;
+#include "btreeprimitivedefinitions.h"
 
-void keySortPrim_add (CBTreeKeySortDataIf<keySortEntry_t, uint32_t> *pClKeySort, uint32_t nEntries, uint32_t &nDebug, uint32_t &nFromWhereOrSeed, btreetest_keysort_primitive_seek_e eWhere);
-void keySortPrim_remove (CBTreeKeySortDataIf<keySortEntry_t, uint32_t> *pClKeySort, uint32_t nEntries, uint32_t nInstance, uint32_t &nFromWhereOrSeed, btreetest_keysort_primitive_seek_e eWhere);
-bool keySortPrim_compare (CBTreeKeySortDataIf<keySortEntry_t, uint32_t> *pClKeySort0, CBTreeKeySortDataIf<keySortEntry_t, uint32_t> *pClKeySort1);
+typedef CBTreePairTest<const uint32_t, keySortMap_t>										keySortPair_t;
+
+void container_data_reset (keySortEntry_t &rEntry);
+void container_data_set (keySortEntry_t &rEntry, uint32_t nDebug, uint32_t &nFromWhereOrSeed, btreetest_key_generation_e eGenerator);
+
+uint32_t get_entry_key (const keySortEntry_t &rEntry);
+uint32_t get_entry_data (const keySortEntry_t &rEntry);
+uint32_t get_entry_debug (const keySortEntry_t &rEntry);
+
+void set_entry_key (keySortEntry_t &rEntry, uint32_t nKey);
+void set_entry_data (keySortEntry_t &rEntry, uint32_t nData);
+void set_entry_debug (keySortEntry_t &rEntry, uint32_t nDebug);
+
+template<class _t_pair>
+void container_data_reset (_t_pair &rEntry)
+{
+	set_entry_key (rEntry, 0);
+	set_entry_data (rEntry, 0);
+	set_entry_debug (rEntry, 0);
+}
+
+template<class _t_pair>
+void container_data_set (_t_pair &rEntry, uint32_t nDebug, uint32_t &nFromWhereOrSeed, btreetest_key_generation_e eGenerator)
+{
+	switch (eGenerator)
+	{
+		case BTREETEST_KEY_GENERATION_DESCEND	:
+		{
+			set_entry_key (rEntry, nFromWhereOrSeed);
+
+			nFromWhereOrSeed--;
+			
+			break;
+		}
+
+		case BTREETEST_KEY_GENERATION_ASCEND	:
+		{
+			set_entry_key (rEntry, nFromWhereOrSeed);
+
+			nFromWhereOrSeed++;
+			
+			break;
+		}
+
+		case BTREETEST_KEY_GENERATION_RANDOM	:
+		{
+			set_entry_key (rEntry, generate_rand32 ());
+
+			break;
+		}
+
+		case BTREETEST_KEY_GENERATION_CONST		:
+		{
+			set_entry_key (rEntry, nFromWhereOrSeed);
+
+			break;
+		}
+
+		default									:
+		{
+			cerr << "ERROR: container_data_set<keySortEntry_>: eGenerator corrupted or not set!" << endl;
+
+			exit (-1);
+
+			break;
+		}
+	}
+
+	set_entry_data (rEntry, generate_rand32 ());
+	set_entry_debug (rEntry, nDebug);
+}
+
+template<class _t_pair>
+uint32_t get_entry_key (const _t_pair &rEntry)
+{
+	return (rEntry.first);
+}
+
+template<class _t_pair>
+uint32_t get_entry_data (const _t_pair &rEntry)
+{
+	return (rEntry.second.nData);
+}
+
+template<class _t_pair>
+uint32_t get_entry_debug (const _t_pair &rEntry)
+{
+	return (rEntry.second.nDebug);
+}
+
+template<class _t_pair>
+void set_entry_key (_t_pair &rEntry, uint32_t nKey)
+{
+	(typename ::std::remove_const<typename _t_pair::first_type>::type &) rEntry.first = nKey;
+}
+
+template<class _t_pair>
+void set_entry_data (_t_pair &rEntry, uint32_t nData)
+{
+	rEntry.second.nData = nData;
+}
+
+template<class _t_pair>
+void set_entry_debug (_t_pair &rEntry, uint32_t nDebug)
+{
+	rEntry.second.nDebug = nDebug;
+}
+
+template<class _t_dest_data, class _t_src_data>
+void entry_conversion (_t_dest_data &rDstData, const _t_src_data &rSrcData)
+{
+	set_entry_key (rDstData, get_entry_key (rSrcData));
+	set_entry_data (rDstData, get_entry_data (rSrcData));
+	set_entry_debug (rDstData, get_entry_debug (rSrcData));
+}
 
 #endif // BTREEKEYSORTTESTPRIMITIVE_H
