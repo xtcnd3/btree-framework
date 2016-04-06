13bfc8e78034d989b60cd6c67e8c252835d3df25
xtcnd3 <btreeframework@gmail.com>
2016-04-06 12:27:22 +0100

    release 0.04a (alpha)

---
+++ src/btreetest/btree_framework/btreeif.cpp
@@ -0,0 +1,61 @@
+/************************************************************
+**
+** file:	btreeif.cpp
+** author:	Andreas Steffens
+** license:	GPL v2
+**
+** description:
+**
+** This file contains code for the b-tree frameworks base class
+** on the iterator interface level.
+**
+************************************************************/
+
+#ifndef	BTREEBASEITERIF_CPP
+#define	BTREEBASEITERIF_CPP
+
+#include "btreeif.h"
+
+template <class _t_data, class _t_sizetype>
+CBTreeIf<_t_data, _t_sizetype>::CBTreeIf ()
+	:	CBTreeSuper ()
+{
+}
+
+template <class _t_data, class _t_sizetype>
+CBTreeIf<_t_data, _t_sizetype>::CBTreeIf (const CBTreeIf<_t_data, _t_sizetype> &rContainer)
+	:	CBTreeSuper ()
+{
+}
+
+template <class _t_data, class _t_sizetype>
+CBTreeIf<_t_data, _t_sizetype>::~CBTreeIf ()
+{
+}
+
+/*
+
+operator= - assignment operator overload
+
+rContainer		- is a reference of a container that will have its content be assigned to this instance
+
+The result is a reference to this instance.
+
+*/
+
+template <class _t_data, class _t_sizetype>
+CBTreeIf<_t_data, _t_sizetype> &
+	CBTreeIf<_t_data, _t_sizetype>::operator=
+	(const CBTreeIf<_t_data, _t_sizetype> &rContainer)
+{
+	// if this is not the same instance as the right hand side container ...
+	if (this != &rContainer)
+	{
+		// ... then assign all data from right hand side to left hand side container
+		this->_assign (rContainer);
+	}
+
+	return (*this);
+}
+
+#endif // BTREEBASEITERIF_CPP
