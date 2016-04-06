6646253d289cdff2c83e3622fda153eca74f08e2
xtcnd3 <btreeframework@gmail.com>
2016-04-06 12:27:23 +0100

    release 0.04a (alpha)

--- src/btreetest/btreemultimapprimitives.cpp
+++ src/btreetest/btreemultimapprimitives.cpp
@@ -16,11 +16,13 @@
 
 #include "btreemultimapprimitives.h"
 
-template<class _t_obj, class _t_value>
-void multiMapPrim_add (_t_obj *pClMM, uint32_t nEntries, uint32_t &nFromWhereOrSeed, btreetest_multimap_primitive_seek_e eWhere)
+template<class _t_container>
+void multiMapPrim_add (_t_container *pClMM, uint32_t nEntries, uint32_t &nFromWhereOrSeed, btreetest_multimap_primitive_seek_e eWhere)
 {
-	uint32_t							nKey;
-	typename _t_value::second_type		sMap = {0, 0};
+	typedef typename _t_container::value_type		data_t;
+
+	typename ::std::remove_const<typename data_t::first_type>::type		nKey;
+	typename data_t::second_type										sMap = {0, 0};
 
 	uint32_t		ui32;
 
@@ -31,9 +33,11 @@ void multiMapPrim_add (_t_obj *pClMM, uint32_t nEntries, uint32_t &nFromWhereOrS
 
 	for (ui32 = 0; ui32 < nEntries; ui32++)
 	{
-		cout << "insert: " << ui32 << " / " << nEntries << "\r" << flush;
+		::std::cout << "insert: " << ui32 << " / " << nEntries << "\r" << ::std::flush;
 
-		sMap.nDebug = 0;
+		sMap.nDebug = g_nDebug;
+
+		g_nDebug++;
 
 		switch (eWhere)
 		{
@@ -81,7 +85,7 @@ void multiMapPrim_add (_t_obj *pClMM, uint32_t nEntries, uint32_t &nFromWhereOrS
 
 		sMap.nData = generate_rand32 ();
 
-		_t_value		sEntry (nKey, sMap);
+		data_t			sEntry (nKey, sMap);
 
 		pClMM->insert (sEntry);
 	}
