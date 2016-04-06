12deb3ca02613c8271cf28cd07c96d26e000a74c
xtcnd3 <btreeframework@gmail.com>
2016-04-06 12:27:22 +0100

    release 0.04a (alpha)

--- src/btreetest/btree_framework/btreeaux.h
+++ src/btreetest/btree_framework/btreeaux.h
@@ -28,9 +28,7 @@ public:
 								CBTreeSuper					();
 								~CBTreeSuper				();
 
-	btree_time_stamp_t			get_time_stamp				();
-
-	void						swap						(CBTreeSuper &rBT);
+	btree_time_stamp_t			get_time_stamp				() const;
 
 protected:
 
@@ -38,31 +36,12 @@ protected:
 
 	uint32_t					create_dataPool				(CBTreeIOperBlockPoolDesc_t *pPoolDesc);
 
+	void						_swap						(CBTreeSuper &rBT);
+
 	uint32_t					m_nNumPools;
 	CBTreeIOperBlockPoolDesc_t	*m_pPoolDesc;
 
 	btree_time_stamp_t			m_sTimeStamp;
-};
-
-template <class _t_sizetype = uint64_t>
-class CBTreeIf
-{
-public:
-
-	// construction
-						CBTreeIf<_t_sizetype>
-													()
-						{
-						};
-						
-	// destruction
-						~CBTreeIf<_t_sizetype>
-													()
-						{
-						};
-
-	virtual bool		empty						() = 0;
-	virtual _t_sizetype	size						() = 0;
 };
 
 #endif // BTREEAUX_H
