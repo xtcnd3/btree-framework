7d6158c20dc8c6c700e4b275bcae6cb8c8ca8fde
xtcnd3 <btreeframework@gmail.com>
2016-04-06 12:27:22 +0100

    release 0.04a (alpha)

---
+++ src/btreetest/btreecommonprimitives.cpp
@@ -0,0 +1,14 @@
+/************************************************************
+**
+** file:	btreecommonprimitives.cpp
+** author:	Andreas Steffens
+** license:	GPL v2
+**
+** description:
+**
+** This file contains primitive code for the b-tree framework's
+** test bench to support any application class testing.
+**
+************************************************************/
+
+#include "btreecommonprimitives.h"
