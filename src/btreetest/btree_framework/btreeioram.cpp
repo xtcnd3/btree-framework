1399ef5b08605d630a550b8bf497e7de7536cbcd
xtcnd3 <btreeframework@gmail.com>
2016-07-04 16:22:54 +0100

    release 0.04b (alpha)

--- src/btreetest/btree_framework/btreeioram.cpp
+++ src/btreetest/btree_framework/btreeioram.cpp
@@ -16,32 +16,32 @@
 
 #include "btreeioram.h"
 
-using namespace std;
+#include "btreeramioprop.h"
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::CBTreeRAMIO
+template<class _t_datalayerproperties>
+CBTreeRAMIO<_t_datalayerproperties>::CBTreeRAMIO
 (
-	CBTreeIOpropertiesRAM &rDataLayerProperties, 
-	_t_addresstype nBlockSize, 
-	_t_subnodeiter nNodeSize, 
+	_t_datalayerproperties &rDataLayerProperties, 
+	typename CBTreeRAMIO<_t_datalayerproperties>::address_type nBlockSize, 
+	typename _t_datalayerproperties::sub_node_iter_type nNodeSize, 
 	uint32_t nNumDataPools, 
 	CBTreeIOperBlockPoolDesc_t *psDataPools
 )
-	:	CBTreeLinearIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype> (nBlockSize, nNumDataPools, psDataPools)
+	:	CBTreeLinearIO<_t_datalayerproperties> (nBlockSize, nNumDataPools, psDataPools)
 {
-	m_pClDataLayerProperties = new CBTreeIOpropertiesRAM (rDataLayerProperties);
+	m_pClDataLayerProperties = new _t_datalayerproperties (rDataLayerProperties);
 
 	BTREEDATA_ASSERT (m_pClDataLayerProperties != NULL, "CBTreeRAMIO::CBTreeRAMIO: insufficient memory!");
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::~CBTreeRAMIO ()
+template<class _t_datalayerproperties>
+CBTreeRAMIO<_t_datalayerproperties>::~CBTreeRAMIO ()
 {
 	delete m_pClDataLayerProperties;
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::get_performance_counters (uint64_t (&rHitCtrs)[PERFCTR_TERMINATOR], uint64_t (&rMissCtrs)[PERFCTR_TERMINATOR])
+template<class _t_datalayerproperties>
+void CBTreeRAMIO<_t_datalayerproperties>::get_performance_counters (uint64_t (&rHitCtrs)[PERFCTR_TERMINATOR], uint64_t (&rMissCtrs)[PERFCTR_TERMINATOR])
 {
 	memset ((void *) &rHitCtrs, 0, sizeof (rHitCtrs));
 	memset ((void *) &rMissCtrs, 0, sizeof (rMissCtrs));
@@ -57,9 +57,9 @@ nEntry[in]	- specifies entry to be returned
 
 */
 
-template<class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+template<class _t_datalayerproperties>
 template<class _t_dl_data>
-_t_dl_data* CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::get_pooledData (uint32_t nPool, _t_nodeiter nNode)
+_t_dl_data* CBTreeRAMIO<_t_datalayerproperties>::get_pooledData (uint32_t nPool, typename _t_datalayerproperties::node_iter_type nNode)
 {
 	_t_dl_data			*psData = (_t_dl_data *) this->get_node_base (nPool, nNode);
 
@@ -76,9 +76,9 @@ nEntry[in]	- specifies entry to be returned
 
 */
 
-template<class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+template<class _t_datalayerproperties>
 template<class _t_dl_data>
-_t_dl_data* CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::get_pooledData (uint32_t nPool, _t_nodeiter nNode, _t_subnodeiter nEntry)
+_t_dl_data* CBTreeRAMIO<_t_datalayerproperties>::get_pooledData (uint32_t nPool, typename _t_datalayerproperties::node_iter_type nNode, typename _t_datalayerproperties::sub_node_iter_type nEntry)
 {
 	const uint8_t		*psPooledNode = this->template get_pooledData<uint8_t> (nPool, nNode);
 	_t_dl_data			*psData = (_t_dl_data *) &(psPooledNode[nEntry * this->get_pool_entry_size (nPool)]);
@@ -98,9 +98,9 @@ pData	- pointer to new data
 
 */
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-template <class _t_dl_data>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::insert_dataIntoPool (uint32_t nPool, _t_nodeiter nNode, _t_subnodeiter nNodeLen, _t_subnodeiter nOffset, _t_subnodeiter nDataLen, const _t_dl_data *pData)
+template<class _t_datalayerproperties>
+template<class _t_dl_data>
+void CBTreeRAMIO<_t_datalayerproperties>::insert_dataIntoPool (uint32_t nPool, typename _t_datalayerproperties::node_iter_type nNode, typename _t_datalayerproperties::sub_node_iter_type nNodeLen, typename _t_datalayerproperties::sub_node_iter_type nOffset, typename _t_datalayerproperties::sub_node_iter_type nDataLen, const _t_dl_data *pData)
 {
 #if defined (_DEBUG)
 
@@ -114,21 +114,21 @@ void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::in
 	memcpy ((void *) &(psNodeData[nOffset]), (void *) pData, sizeof (*psNodeData) * nDataLen);
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::set_size (_t_nodeiter nMaxNodes)
+template<class _t_datalayerproperties>
+void CBTreeRAMIO<_t_datalayerproperties>::set_size (typename _t_datalayerproperties::node_iter_type nMaxNodes)
 {
 	if (nMaxNodes == this->m_nMaxNodes)
 	{
 		return;
 	}
 
-	_t_addresstype		nSize;
+	address_type		nSize;
 
 #if defined (_DEBUG)
 	
-	_t_addresstype		nOrgSize;
+	address_type		nOrgSize;
 
-	_t_nodeiter			bNotInit;
+	node_iter_type		bNotInit;
 
 #endif
 
@@ -142,7 +142,7 @@ void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::se
 
 #endif
 
-		nSize = (_t_addresstype) (this->get_pool_total_size (nPool) * (nMaxNodes + 1));
+		nSize = (address_type) (this->get_pool_total_size (nPool) * (nMaxNodes + 1));
 
 		this->m_ppsPools[nPool] = (uint8_t *) realloc ((void *) (this->m_ppsPools[nPool]), (size_t) nSize);
 
@@ -152,7 +152,7 @@ void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::se
 
 		if (nMaxNodes > this->m_nMaxNodes)
 		{
-			nOrgSize = (_t_addresstype) (this->get_pool_total_size (nPool) * (this->m_nMaxNodes + bNotInit));
+			nOrgSize = (address_type) (this->get_pool_total_size (nPool) * (this->m_nMaxNodes + bNotInit));
 
 			memset ((void *) &(((this->m_ppsPools[nPool]))[nOrgSize]), 0xEE, (size_t) (nSize - nOrgSize));
 		}
@@ -163,161 +163,158 @@ void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::se
 	this->m_nMaxNodes = nMaxNodes;
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::unload ()
+template<class _t_datalayerproperties>
+void CBTreeRAMIO<_t_datalayerproperties>::unload ()
 {
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::unload_from_cache (_t_nodeiter nNode)
+template<class _t_datalayerproperties>
+void CBTreeRAMIO<_t_datalayerproperties>::unload_from_cache (typename _t_datalayerproperties::node_iter_type nNode)
 {
 	nNode;
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-bool CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::is_dataCached (uint32_t nPool, _t_nodeiter nNode)
+template<class _t_datalayerproperties>
+bool CBTreeRAMIO<_t_datalayerproperties>::is_dataCached (uint32_t nPool, typename _t_datalayerproperties::node_iter_type nNode)
 {
 	bool	bRetval = false;
 
 	return (bRetval);
 }
 
-template <class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
-void CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::showdump	(std::ofstream &ofs, _t_nodeiter nTreeSize, char *pAlloc)
+template<class _t_datalayerproperties>
+void CBTreeRAMIO<_t_datalayerproperties>::showdump	(std::ofstream &ofs, typename _t_datalayerproperties::node_iter_type nTreeSize, char *pAlloc)
 {
-	_t_nodeiter		ui64;
+	node_iter_type	ui64;
 	uint32_t		ui32;
 
 	this->set_cacheFreeze (true);
 	{
-		ofs << "<H1>data dump</H1>" << endl;
-		ofs << "<br>" << endl;
+		ofs << "<H1>data dump</H1>" << ::std::endl;
+		ofs << "<br>" << ::std::endl;
 
-		ofs << "m_nNumDataPools: " << this->m_nNumDataPools << "<br>" << endl;
+		ofs << "m_nNumDataPools: " << this->m_nNumDataPools << "<br>" << ::std::endl;
 		
-		ofs << "<table>" << endl;
+		ofs << "<table>" << ::std::endl;
 		
-		ofs << "<tr>" << endl;
-		ofs << "<td></td>" << endl;
+		ofs << "<tr>" << ::std::endl;
+		ofs << "<td></td>" << ::std::endl;
 
 		for (ui32 = 0; ui32 < this->m_nNumDataPools; ui32++)
 		{
-			ofs << "<td>Pool " << ui32 << "</td>" << endl;
+			ofs << "<td>Pool " << ui32 << "</td>" << ::std::endl;
 		}
 
-		ofs << "</tr>" << endl;
+		ofs << "</tr>" << ::std::endl;
 
-		ofs << "<tr>" << endl;
-		ofs << "<td>m_psDataPools->nEntrySize</td>" << endl;
+		ofs << "<tr>" << ::std::endl;
+		ofs << "<td>m_psDataPools->nEntrySize</td>" << ::std::endl;
 
 		for (ui32 = 0; ui32 < this->m_nNumDataPools; ui32++)
 		{
-			ofs << "<td>" << this->m_psDataPools[ui32].nEntrySize << "</td>" << endl;
+			ofs << "<td>" << this->m_psDataPools[ui32].nEntrySize << "</td>" << ::std::endl;
 		}
 
-		ofs << "</tr>" << endl;
+		ofs << "</tr>" << ::std::endl;
 
-		ofs << "<tr>" << endl;
-		ofs << "<td>m_psDataPools->nTotalSize</td>" << endl;
+		ofs << "<tr>" << ::std::endl;
+		ofs << "<td>m_psDataPools->nTotalSize</td>" << ::std::endl;
 
 		for (ui32 = 0; ui32 < this->m_nNumDataPools; ui32++)
 		{
-			ofs << "<td>" << this->m_psDataPools[ui32].nTotalSize << "</td>" << endl;
+			ofs << "<td>" << this->m_psDataPools[ui32].nTotalSize << "</td>" << ::std::endl;
 		}
 
-		ofs << "</tr>" << endl;
+		ofs << "</tr>" << ::std::endl;
 		
-		ofs << "</table>" << endl;
+		ofs << "</table>" << ::std::endl;
 
-		ofs << "<table>" << endl;
+		ofs << "<table>" << ::std::endl;
 
 		for (ui64 = 0; ui64 < nTreeSize; ui64++)
 		{
-			ofs << "<tr>" << endl;
-			ofs << "<td>" << endl;
+			ofs << "<tr>" << ::std::endl;
+			ofs << "<td>" << ::std::endl;
 
 			ofs << "<a name=\"node_dump_" << ui64 << "\">";
 			ofs << "node: " << ui64;
-			ofs << "</a>" << endl;
+			ofs << "</a>" << ::std::endl;
 
-			ofs << "</td><td><a href=\"#node_" << ui64 << "\">tree<a/></td><td><a href=\"#node_list_" << ui64 << "\">list<a/></td>" << endl;
-			ofs << "</tr>" << endl;
+			ofs << "</td><td><a href=\"#node_" << ui64 << "\">tree<a/></td><td><a href=\"#node_list_" << ui64 << "\">list<a/></td>" << ::std::endl;
+			ofs << "</tr>" << ::std::endl;
 
 			{
-				ofs << "<tr>" << endl;
+				ofs << "<tr>" << ::std::endl;
 
-				ofs << "<td></td><td>" << endl;
+				ofs << "<td></td><td>" << ::std::endl;
 
 				uint32_t	uk32;
 
 				for (uk32 = 0; uk32 < this->m_nNumDataPools; uk32++)
 				{
-					_t_addresstype	nAddr = (_t_addresstype) (this->get_pool_total_size (uk32) * ui64);
+					address_type	nAddr = (address_type) (this->get_pool_total_size (uk32) * ui64);
 					bool			bIsCached = this->is_dataCached (uk32, ui64);
 
-					ofs << "<td>" << endl;
+					ofs << "<td>" << ::std::endl;
 					
 					if (!bIsCached)
 					{
 						ofs << "<font color=\"#999999\">";
 					}
 
-					ofs << nAddr << ":" << endl;
+					ofs << nAddr << ":" << ::std::endl;
 					
 					if (!bIsCached)
 					{
 						ofs << "</font>";
 					}
 
-					ofs << "</td>" << endl;
+					ofs << "</td>" << ::std::endl;
 
-					ofs << "<td>" << endl;
+					ofs << "<td>" << ::std::endl;
+
+					uint32_t	ul32;
+					uint8_t		*pb;
+					uint32_t	nMaxPoolLen;
 
 					if (bIsCached)
 					{
-						uint32_t	ul32;
-						uint8_t		*pb;
-						uint32_t	nMaxPoolLen;
-
 						ofs << "<font color=\"#4040FF\">";
-
-						nMaxPoolLen = this->get_pool_total_size (uk32);
-
-						pb = new uint8_t [nMaxPoolLen];
-
-						BTREEDATA_ASSERT (pb != NULL, "CBTreeRAMIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::showdump: insufficient memory!");
-
-						pb = this->template get_pooledData<uint8_t> (uk32, ui64);
-					
-						for (ul32 = 0; ul32 < nMaxPoolLen; ul32++)
-						{
-							ofs << hex << setfill ('0') << setw (2) << (uint32_t) pb[ul32] << dec;
-
-							if (((ul32 + 1) % this->get_pool_entry_size (uk32)) == 0)
-							{
-								ofs << "<br>" << endl;
-							}
-							else if (((ul32 + 1) % 4) == 0)
-							{
-								ofs << " ";
-							}
-						}
-
-						delete [] pb;
-
-						ofs << "</font>";
-
-						ofs << "<br>";
 					}
+
+					nMaxPoolLen = this->get_pool_total_size (uk32);
+
+					pb = this->template get_pooledData<uint8_t> (uk32, ui64);
+					
+					for (ul32 = 0; ul32 < nMaxPoolLen; ul32++)
+					{
+						ofs << ::std::hex << ::std::setfill ('0') << ::std::setw (2) << (uint32_t) pb[ul32] << ::std::dec;
+
+						if (((ul32 + 1) % this->get_pool_entry_size (uk32)) == 0)
+						{
+							ofs << "<br>" << ::std::endl;
+						}
+						else if (((ul32 + 1) % 4) == 0)
+						{
+							ofs << " ";
+						}
+					}
+
+					if (bIsCached)
+					{
+						ofs << "</font>";
+					}
+
+					ofs << "<br>";
 					
 					ofs << "</td>";
 				}
 
-				ofs << "</tr>" << endl;
+				ofs << "</tr>" << ::std::endl;
 			}
 		}
 
-		ofs << "</table>" << endl;
+		ofs << "</table>" << ::std::endl;
 	}
 	this->set_cacheFreeze (false);
 }
