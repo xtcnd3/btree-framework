a8570311a447837159ba8cf06578d73035dbde0a
xtcnd3 <btreeframework@gmail.com>
2016-07-04 16:22:55 +0100

    release 0.04b (alpha)

---
+++ src/btreetest/btreetestarrayperf.h
@@ -0,0 +1,16 @@
+
+#ifndef	BTREETESTARRAYPERF_H
+#define	BTREETESTARRAYPERF_H
+
+#include <stdint.h>
+
+#include <list>
+#include <vector>
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <btreearray.h>
+
+#endif
