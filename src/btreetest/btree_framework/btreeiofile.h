2a6ce2e427324d79bb5a9c802217c9b320240ea8
xtcnd3 <btreeframework@gmail.com>
2016-07-04 16:22:54 +0100

    release 0.04b (alpha)

--- src/btreetest/btree_framework/btreeiofile.h
+++ src/btreetest/btree_framework/btreeiofile.h
@@ -14,12 +14,12 @@
 #ifndef	BTREEFILEIO_H
 #define	BTREEFILEIO_H
 
-#if defined (WIN32)
+#if defined (_MSC_VER)
 
  #include <io.h>
  #include <windows.h>
 
-#elif defined (LINUX)
+#elif defined(__GNUC__) || defined(__GNUG__)
 
  #include <unistd.h>
  #include <sys/mman.h>
@@ -35,53 +35,59 @@
 
 #include "btreeioblock.h"
 #include "btreeaux.h"
-#include "btreefileioprop.h"
 
-template <class _t_nodeiter = uint64_t, class _t_subnodeiter = uint32_t, class _t_addresstype = uint64_t, class _t_offsettype = uint32_t>
-class CBTreeFileIO : public CBTreeBlockIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+template<class _t_datalayerproperties>
+class CBTreeFileIO : public CBTreeBlockIO<_t_datalayerproperties>
 {
 public:
+	
+	typedef typename _t_datalayerproperties::size_type				size_type;
+	typedef typename _t_datalayerproperties::node_iter_type			node_iter_type;
+	typedef typename _t_datalayerproperties::sub_node_iter_type		sub_node_iter_type;
+	typedef typename _t_datalayerproperties::address_type			address_type;
+	typedef typename _t_datalayerproperties::offset_type			offset_type;
+
 	// construction
-						CBTreeFileIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+						CBTreeFileIO<_t_datalayerproperties>
 												(
-													CBTreeIOpropertiesFile &rDataLayerProperties, 
-													_t_addresstype nBlockSize, 
-													_t_subnodeiter nNodeSize,
+													_t_datalayerproperties &rDataLayerProperties, 
+													address_type nBlockSize, 
+													sub_node_iter_type nNodeSize,
 													uint32_t nNumDataPools, 
 													CBTreeIOperBlockPoolDesc_t *psDataPools
 												);
 
-						~CBTreeFileIO<_t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+						~CBTreeFileIO<_t_datalayerproperties>
 												();
 
 	// monitoring
-	void				get_performance_counters				(uint64_t (&rHitCtrs)[PERFCTR_TERMINATOR], uint64_t (&rMissCtrs)[PERFCTR_TERMINATOR]);
+	void				get_performance_counters	(uint64_t (&rHitCtrs)[PERFCTR_TERMINATOR], uint64_t (&rMissCtrs)[PERFCTR_TERMINATOR]);
 
 	// data access primitives
 	template<class _t_dl_data>
-	inline _t_dl_data *	get_pooledData				(uint32_t nPool, _t_nodeiter nNode);
+	inline _t_dl_data *	get_pooledData				(uint32_t nPool, node_iter_type nNode);
 
 	template<class _t_dl_data>
-	inline _t_dl_data *	get_pooledData				(uint32_t nPool, _t_nodeiter nNode, _t_subnodeiter nEntry);
+	inline _t_dl_data *	get_pooledData				(uint32_t nPool, node_iter_type nNode, sub_node_iter_type nEntry);
 
 	// mid level data access
 	template<class _t_dl_data>
-	void				insert_dataIntoPool			(uint32_t nPool, _t_nodeiter nNode, _t_subnodeiter nNodeLen, _t_subnodeiter nOffset, _t_subnodeiter nDataLen, const _t_dl_data *pData);
+	void				insert_dataIntoPool			(uint32_t nPool, node_iter_type nNode, sub_node_iter_type nNodeLen, sub_node_iter_type nOffset, sub_node_iter_type nDataLen, const _t_dl_data *pData);
 
 	// resources
-	void				set_size					(_t_nodeiter nMaxNodes);
+	void				set_size					(node_iter_type nMaxNodes);
 	void				unload						();
 
 	// cache management
-	void				unload_from_cache			(_t_nodeiter nNode);
+	void				unload_from_cache			(node_iter_type nNode);
 
 	// cache information
-	bool				is_dataCached				(uint32_t nPool, _t_nodeiter nNode);
+	bool				is_dataCached				(uint32_t nPool, node_iter_type nNode);
 
 	// resource management
 	void				terminate_access			();
 
-	void				showdump					(std::ofstream &ofs, _t_nodeiter nTreeSize, char *pAlloc);
+	void				showdump					(std::ofstream &ofs, node_iter_type nTreeSize, char *pAlloc);
 
 protected:
 
@@ -94,20 +100,24 @@ protected:
 
 	void				unmap_all_descriptors		(bool bExceptRoot);
 
-	string											m_strTempFile;
+	::std::string									m_strTempFile;
 
-#if defined (WIN32)
+#if defined (_MSC_VER)
+
 	HANDLE											m_hFile;
 	HANDLE											m_hFileMapping;
-#elif defined (LINUX)
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+
 	int												m_nFileDesc;
+
 #endif
 
-	CBTreeIOpropertiesFile							*m_pClDataLayerProperties;
+	_t_datalayerproperties							*m_pClDataLayerProperties;
 
 	bool											m_bNoMapHandling;
 
-	_t_addresstype									m_nTotalAddressSpace;
+	address_type									m_nTotalAddressSpace;
 };
 
 #endif // BTREEFILEIO_H
