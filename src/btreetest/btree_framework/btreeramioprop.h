067df1e161d8877ce0d67d34cf190e8c42553f3a
xtcnd3 <btreeframework@gmail.com>
2016-07-04 16:22:54 +0100

    release 0.04b (alpha)

--- src/btreetest/btree_framework/btreeramioprop.h
+++ src/btreetest/btree_framework/btreeramioprop.h
@@ -16,21 +16,41 @@
 
 #include <stdint.h>
 
+#include <type_traits>
+
 #include "btreecommon.h"
 #include "btreeioprop.h"
+#include "btreeioram.h"
 
-class CBTreeIOpropertiesRAM : public CBTreeIOproperties
+template<class _t_sizetype = uint64_t, class _t_nodeiter = uint64_t, class _t_subnodeiter = uint32_t, class _t_addresstype = uint64_t, class _t_offsettype = uint32_t>
+class CBTreeIOpropertiesRAM : public CBTreeIOproperties<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
 {
 public:
 
-					CBTreeIOpropertiesRAM		();
+	typedef CBTreeRAMIO<CBTreeIOpropertiesRAM<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype> >
+															data_layer_type;
 
-					CBTreeIOpropertiesRAM		(CBTreeIOpropertiesRAM &rBT);
+	typedef _t_sizetype										size_type;
+	typedef _t_nodeiter										node_iter_type;
+	typedef _t_subnodeiter									sub_node_iter_type;
+	typedef _t_addresstype									address_type;
+	typedef _t_offsettype									offset_type;
 
-					~CBTreeIOpropertiesRAM		();
+	typedef ::std::false_type								has_latency;
+
+					CBTreeIOpropertiesRAM<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+												();
+
+					CBTreeIOpropertiesRAM<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+												(CBTreeIOpropertiesRAM &rBT);
+
+					~CBTreeIOpropertiesRAM<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>
+												();
 
 protected:
 
 };
 
 #endif // BTREEIORAMPROP_H
+
+#include "btreeramioprop.cpp"
