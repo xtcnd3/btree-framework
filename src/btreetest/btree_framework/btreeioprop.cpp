1a3d9a27cb1b7c10385c7ae9adb4ee95617ae67b
xtcnd3 <btreeframework@gmail.com>
2016-07-04 16:22:54 +0100

    release 0.04b (alpha)

--- src/btreetest/btree_framework/btreeioprop.cpp
+++ src/btreetest/btree_framework/btreeioprop.cpp
@@ -11,12 +11,19 @@
 **
 ************************************************************/
 
+#ifndef BTREEIOPROP_CPP
+#define BTREEIOPROP_CPP
+
 #include "btreeioprop.h"
 
-CBTreeIOproperties::CBTreeIOproperties ()
+template<class _t_sizetype, class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+CBTreeIOproperties<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::CBTreeIOproperties ()
 {
 }
 
-CBTreeIOproperties::~CBTreeIOproperties ()
+template<class _t_sizetype, class _t_nodeiter, class _t_subnodeiter, class _t_addresstype, class _t_offsettype>
+CBTreeIOproperties<_t_sizetype, _t_nodeiter, _t_subnodeiter, _t_addresstype, _t_offsettype>::~CBTreeIOproperties ()
 {
 }
+
+#endif // BTREEIOPROP_CPP
