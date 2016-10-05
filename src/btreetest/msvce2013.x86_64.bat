08837050052c40d72c12d9f6723a10a28b277212
xtcnd3 <btreeframework@gmail.com>
2016-10-05 09:17:40 +0100

    build system has been switched to cmake

---
+++ src/btreetest/msvce2013.x86_64.bat
@@ -0,0 +1,23 @@
+# get Microsoft Visual Studio command from registery
+for /f "tokens=3*" %%i in ('reg query "HKEY_CLASSES_ROOT\Applications\WDExpress.exe\shell\open\command" /ve') do set VC_PATH_TMP=%%i %%j
+
+# filter argument placeholder
+set VC_PATH_LOCAL=%VC_PATH_TMP:"%1"=%
+
+set WORKING_DIR=%CD%
+
+mkdir cmake
+cd cmake
+cmake.exe -G "Visual Studio 12" -A "x64" ..
+
+if errorlevel 1 (
+	echo "cmake failed to generate the project files"
+	echo "Note: The windows version of cmake is required and NOT the cygwin version!"
+	echo "The cygwin version doesn't have any Visual Studio generators compiled in!"
+	pause
+	exit 1
+)
+
+if exist %VC_PATH_LOCAL% (
+	start "" %VC_PATH_LOCAL% %WORKING_DIR%/cmake/btreetest.sln
+)
